05469d948e41c3d9640bf765b187c3c5bccd4c41
Inwon Kim <inwonkim@iconloop.com>
2023-08-01 11:51:01 +0900

    pageNation

--- app/components/PageNation.tsx
+++ app/components/PageNation.tsx
@@ -1,28 +1,36 @@
+'use client'
 import Link from "next/link";
+import {useSearchParams} from "next/navigation";
 
 export function PageNation() {
+    const searchParams = useSearchParams()!;
+    const params = new URLSearchParams(searchParams.toString());
+    const queryObject = Object.fromEntries(params);
+    delete queryObject["page"];
+    const p = parseInt(params.get("page") as string || "1");
+    const commonClass = "flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 ";
+    const linkClass = `${commonClass}hover:bg-gray-100 hover:text-gray-700`;
+    const prev = p - 1 > 0 ? <Link href={{pathname: "/messages", query: {...queryObject, "page": p-1}}} title="prev page" className={linkClass}>&#60;</Link> :
+        <span className={commonClass}>&#60;</span>;
     return (
         <ul className="flex h-8 text-sm">
             <li>
-                <Link href="#" title="first page"
-                      className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">&#60;&#60;</Link>
+                <Link href={{pathname: "/messages", query: {...queryObject, "page": "1"}}} title="first page"
+                      className={linkClass}>&#60;&#60;</Link>
             </li>
             <li>
-                <Link href="#" title="prev page"
-                      className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">&#60;</Link>
+                {prev}
             </li>
             <li>
-                <Link href="#" aria-current="page"
-                      className="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700">3
-                    page</Link>
+                <span className={commonClass}>{p}page</span>
             </li>
             <li>
-                <Link href="#" title="next page"
-                      className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">&#62;</Link>
+                <Link href={{pathname: "/messages", query: {...queryObject, "page": p+1}}} title="next page"
+                      className={linkClass}>&#62;</Link>
             </li>
             <li>
-                <Link href="#" title="last page"
-                      className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700">&#62;&#62;</Link>
+                <Link href={{pathname: "/messages", query: {...queryObject, "page": "5"}}} title="last page"
+                      className={linkClass}>&#62;&#62;</Link>
             </li>
         </ul>
     )
--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -6,9 +6,15 @@ const statusOptions = ["status", "SEND", "RECEIVE"];
 const networkOptions = ["network", "0xaa36a7.eth2", "0x61.bsc", "0x111.icon"];
 const limitOptions = ["25", "50", "100"];
 export default async function Page({searchParams}: {searchParams: {[key: string]: string | string[] | undefined}}) {
-    const req = getReqURL(searchParams);
-    const r = await fetch(req, {cache: 'no-store'});
-    const j = await r.json();
+    const net = ensureOptionValue(searchParams["network"] as string, networkOptions);
+    const status = ensureOptionValue(searchParams["status"] as string, statusOptions);
+    const limitParam = ensureOptionValue(searchParams["limit"] as string, limitOptions);
+    const limit = limitParam ? limitParam : limitOptions[0];
+    const pageParamNum = parseInt(searchParams["page"] as string || "1");
+    const page = pageParamNum > 0 ? pageParamNum : 1;
+    const req = `${process.env.API_URI}/api/v1/status?page=${page}&limit=${limit}${net ? "&network=" + net : ""}${status ? "&status=" + status : ""}`;
+    const res = await fetch(req, {cache: 'no-store'});
+    const j = await res.json();
     const rows = j["rows"] as Message[];
     return (
         <section>
@@ -30,16 +36,6 @@ export default async function Page({searchParams}: {searchParams: {[key: string]
             </div>
         </section>
     )
-}
-
-function getReqURL(searchParams: {[key: string]: string | string[] | undefined}) {
-    const net = ensureOptionValue(searchParams["network"] as string, networkOptions);
-    const status = ensureOptionValue(searchParams["status"] as string, statusOptions);
-    const limitParam = ensureOptionValue(searchParams["limit"] as string, limitOptions);
-    const limit = limitParam ? limitParam : limitOptions[0];
-    const pageParamNum = parseInt(searchParams["page"] as string || "1");
-    const page = pageParamNum > 0 ? pageParamNum : 1;
-    return `${process.env.API_URI}/api/v1/status?page=${page}&limit=${limit}${net ? "&network=" + net : ""}${status ? "&status=" + status : ""}`;
 }
 
 function ensureOptionValue(value: string, options: string[]) {
