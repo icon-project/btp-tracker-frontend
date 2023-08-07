b5e6c7eb1f2baee884e8b744a7a19638038a40b1
Inwon Kim <inwonkim@iconloop.com>
2023-08-07 15:49:02 +0900

    Validate nsn input

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -21,8 +21,14 @@ export default function SearchForm({options}: {options: {networkAddress: string}
                         </option>
                     ))}
                 </select>
-                <input type="text"
-                       className="bg-hsla rounded-lg block pl-10 p-2.5 focus:bg-[#f0ffff] w-1/4 mr-1 placeholder:text-2xl placeholder:text-gray-150" placeholder="NSN" required id={"searchInput"}/>
+                <input type="text" pattern="0|[1-9][0-9]*"
+                       className="bg-hsla rounded-lg block pl-10 p-2.5 focus:bg-[#f0ffff] w-1/4 mr-1 placeholder:text-2xl placeholder:text-gray-150"
+                       placeholder="NSN"
+                       required
+                       id={"searchInput"}
+                       onInvalid={e => (e.target as HTMLInputElement).setCustomValidity("Enter valid NSN")}
+                       onInput={e => (e.target as HTMLInputElement).setCustomValidity("")}
+                />
                 <button type="submit"
                         className="h-full p-2.5 text-xl font-medium text-white bg-hsla rounded-lg">
                     <svg className="w-5 h-7" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
