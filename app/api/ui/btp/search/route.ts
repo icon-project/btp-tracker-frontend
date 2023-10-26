8aeb74f3d9765eaf77b1f4390336f90584eae677
by.kim <by.kim@parametacorp.com>
2023-10-26 16:20:36 +0900

    remove mock data and network icon images

--- app/api/ui/btp/search/route.ts
+++
@@ -1,9 +0,0 @@
-import {getMessage} from "@/app/api/message";
-import {NextResponse} from "next/server";
-
-export async function GET(request: Request) {
-    const { searchParams, search } = new URL(request.url);
-    const src = searchParams.get("source");
-    const nsn = parseInt(searchParams.get("nsn") || "1");
-    return NextResponse.json(getMessage(nsn, src));
-}
